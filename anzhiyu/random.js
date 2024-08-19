a20a5493f6771770a03b9dc61439251bace66cd9
CodingOY <132127319+CodingOY@users.noreply.github.com>
2024-08-19 04:41:23 +0000

    update Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["2024/07/30/借教室/","2024/07/29/岛屿个数题解/","2024/07/24/算法笔记（一）/","2024/08/18/算法笔记（二）/"];function toRandomPost(){
+var posts=["2024/07/24/算法笔记（一）/","2024/08/18/算法笔记（二）/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
